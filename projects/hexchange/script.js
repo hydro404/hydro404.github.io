7fcdd6734a90ff2e344f846f34c8453293d65dbd
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-09-03 00:33:57 +0800

    Hexchange

---
+++ projects/hexchange/script.js
@@ -0,0 +1,23 @@
+const colors = ["green", "red", "silver", "gray", "white", "maroon","purple", "fuchsia", "lime", "olive", "navy", "blue", "teal", "aqua"
+
+
+
+
+
+
+
+
+];
+const btn = document.getElementById("btn");
+//const color = document.getElementById("color");
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+function myFunction() {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  document.getElementById("color").innerHTML = colors[randomNumber];
+}
+
