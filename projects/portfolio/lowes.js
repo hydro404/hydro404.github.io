d57a2cfc21c3573fcd854a15adfb17683f475814
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-09-23 12:36:27 +0800

    Updated Landing Page

--- projects/portfolio/lowes.js
+++ projects/portfolio/lowes.js
@@ -1,4 +1,4 @@
-var text = ["Hola", "Konnichiwa", "Annyeong", "Hallo", "Namastē", "Marhaba", "Grüß Gott", "Namaskar", "Zdraveite", "Hafa adai", "Dobro Jutro", "God dag"];
+var text = ["Hola", "Konnichiwa", "Annyeong", "Hallo", "Namastē", "Nǐn hǎo", "Ciao", "Yassas", "Zdraveite", "Hafa adai", "Dobro Jutro", "God dag", "Kamusta"];
 var counter = 0;
 var inst = setInterval(change, 500);
 
@@ -10,10 +10,3 @@ if (counter >= text.length) {
     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
 }
 }
-
-
-function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
-}
-
