a6c863d6bddb6a5e39658b79c378eb989d9e6078
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-10-02 00:45:21 +0800

    Home Page update!
    Changed the font to retro
    Alligned the nav-links
    Revised Responsive Media Query

--- projects/portfolio/script.js
+++ projects/portfolio/script.js
@@ -14,7 +14,7 @@ function clickedAbout() {
 
     else {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 10rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
     
         setTimeout(function(){
         window.location.href = './about.html';
@@ -25,9 +25,18 @@ function clickedAbout() {
 
 function clickedIndex() {
 
-    if (window.matchMedia("(max-width: 768px)").matches) {
+    if (window.matchMedia("(height <= 800px)").matches) {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        
+        setTimeout(function(){
+        window.location.href = './index.html';
+            }, 2000);
+    }
+    
+    else if (window.matchMedia("(max-width: 768px)").matches) {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         
         setTimeout(function(){
         window.location.href = './index.html';
@@ -36,7 +45,7 @@ function clickedIndex() {
 
     else {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 10rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         setTimeout(function(){
             window.location.href = './index.html';
                 }, 2000);
