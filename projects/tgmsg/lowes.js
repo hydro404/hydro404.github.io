3e5166913ba5f9273d829ff2c1843848994834b9
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-10-02 03:18:34 +0800

    Framework Change

---
+++ projects/tgmsg/lowes.js
@@ -0,0 +1,42 @@
+function myFunction() {
+
+
+    
+    var user = document.querySelector('input[name="Username"]').value;
+    var msg = document.querySelector('textarea[name="Message"]').value;
+
+    if(user){
+        if(msg){
+           
+
+            var token = "5419783959:AAGslCXq1N3T1aRQi9apkVGpz9KdZXAdYjQ";
+
+            console.log(msg);
+
+            const params = new URLSearchParams({
+                chat_id: "-769046544",
+                text: msg,
+            });
+
+            //msg.replace("%0A", "%0D%0A");
+
+            var url = 'https://api.telegram.org/bot'+ token+ '/sendMessage?' + params.toString() + "%0D%0A %0D%0A" +  "- " + user;
+            
+
+            var oReq = new XMLHttpRequest();
+            oReq.open("GET", url, true);
+            oReq.send();
+        }
+        else{
+            //ECHO ENTER A MESSAGE
+        }
+    }
+    else{
+        //ECHO ENTER A USERNAME
+    }   
+    
+}; 
+
+
+
+    
