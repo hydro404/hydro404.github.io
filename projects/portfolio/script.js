3d740b935b7fdea551071b57a00320ab49ec3842
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-10-07 04:04:04 +0800

    Revision of About Page

--- projects/portfolio/script.js
+++ projects/portfolio/script.js
@@ -1,25 +1,33 @@
 
 
 function clickedAbout() {
-    
 
-    if (window.matchMedia("(max-width: 768px)").matches) {
+    if (window.matchMedia("(height <= 800px)").matches) {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
-    
+        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        
         setTimeout(function(){
         window.location.href = './about.html';
-        }, 2000);
+            }, 2000);
+    }
+    
+    else if (window.matchMedia("(max-width: 768px)").matches) {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        
+        setTimeout(function(){
+        window.location.href = './about.html';
+            }, 2000);
     }
 
     else {
         document.getElementById("lowes").innerHTML = 
         "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
-    
         setTimeout(function(){
-        window.location.href = './about.html';
-        }, 2000);
-    }
+            window.location.href = './about.html';
+                }, 2000);
+        }
+
     
 }
 
@@ -48,6 +56,35 @@ function clickedIndex() {
         "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         setTimeout(function(){
             window.location.href = './index.html';
+                }, 2000);
+        }
+}
+
+function clickedContact() {
+
+    if (window.matchMedia("(height <= 800px)").matches) {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        
+        setTimeout(function(){
+        window.location.href = './contact.html';
+            }, 2000);
+    }
+    
+    else if (window.matchMedia("(max-width: 768px)").matches) {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        
+        setTimeout(function(){
+        window.location.href = './contact.html';
+            }, 2000);
+    }
+
+    else {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        setTimeout(function(){
+            window.location.href = './contact.html';
                 }, 2000);
         }
 }
