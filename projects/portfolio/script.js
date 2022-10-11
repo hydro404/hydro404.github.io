c901a6f1c770cd9829f415773ace988f6610f189
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-10-12 01:18:35 +0800

    Added Contact Page

--- projects/portfolio/script.js
+++ projects/portfolio/script.js
@@ -2,18 +2,9 @@
 
 function clickedAbout() {
 
-    if (window.matchMedia("(height <= 800px)").matches) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
-        
-        setTimeout(function(){
-        window.location.href = './about.html';
-            }, 2000);
-    }
-    
-    else if (window.matchMedia("(max-width: 768px)").matches) {
-        document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 11.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         
         setTimeout(function(){
         window.location.href = './about.html';
@@ -22,29 +13,19 @@ function clickedAbout() {
 
     else {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 9.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         setTimeout(function(){
             window.location.href = './about.html';
                 }, 2000);
-        }
+    }
 
-    
 }
 
 function clickedIndex() {
-
-    if (window.matchMedia("(height <= 800px)").matches) {
-        document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
-        
-        setTimeout(function(){
-        window.location.href = './index.html';
-            }, 2000);
-    }
     
-    else if (window.matchMedia("(max-width: 768px)").matches) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 11.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         
         setTimeout(function(){
         window.location.href = './index.html';
@@ -53,7 +34,7 @@ function clickedIndex() {
 
     else {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 9.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         setTimeout(function(){
             window.location.href = './index.html';
                 }, 2000);
@@ -62,18 +43,9 @@ function clickedIndex() {
 
 function clickedContact() {
 
-    if (window.matchMedia("(height <= 800px)").matches) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
-        
-        setTimeout(function(){
-        window.location.href = './contact.html';
-            }, 2000);
-    }
-    
-    else if (window.matchMedia("(max-width: 768px)").matches) {
-        document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 11rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 11.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         
         setTimeout(function(){
         window.location.href = './contact.html';
@@ -82,9 +54,9 @@ function clickedContact() {
 
     else {
         document.getElementById("lowes").innerHTML = 
-        "<div style='min-height: calc(100vh - 9rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        "<div style='min-height: calc(100vh - 9.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
         setTimeout(function(){
             window.location.href = './contact.html';
                 }, 2000);
-        }
+    }
 }
