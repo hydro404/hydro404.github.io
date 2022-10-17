5c55a136b07a707e20c5ec1f20379755e830cf66
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-10-18 03:16:55 +0800

    Revising Projects Page

--- projects/portfolio/script.js
+++ projects/portfolio/script.js
@@ -60,3 +60,23 @@ function clickedContact() {
                 }, 2000);
     }
 }
+
+function clickedProjects() {
+
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 11.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        
+        setTimeout(function(){
+        window.location.href = './projects.html';
+            }, 2000);
+    }
+
+    else {
+        document.getElementById("lowes").innerHTML = 
+        "<div style='min-height: calc(100vh - 9.5rem); display: grid; place-items: center;'><button disabled type='button' class='lbutton lbutton--loading' onclick='this.classList.toggle('lbutton--loading')'><span class='button__text'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></button></div>"
+        setTimeout(function(){
+            window.location.href = './projects.html';
+                }, 2000);
+    }
+}
